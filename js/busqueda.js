4cfdec2111933012edeebb0c1be1a56eff3c2856
Sebastian-Q <20203tn049@utez.edu.mx>
2021-06-25 16:34:52 -0500

    Examen de la Unidad 2 Finalizado Lamento la Demora

---
+++ js/busqueda.js
@@ -0,0 +1,4 @@
+const mensaje = () =>{
+    let alerta = document.getElementById("busqueda").value;
+    alert(`La busqueda con nombre ${alerta} no se encuentra, le pido que busque después o nunca gracias XD`);
+}
